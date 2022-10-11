9627f7f697fc84c22719d3a9f9c8fe650c891945
Tri-Nguyen-C0822g1 <tringuyen10396@gmail.com>
2022-10-11 17:28:03 +0700

    add project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Fan fan1 = new Fan(3,false, 10, "yellow");
+        Fan fan2 = new Fan(2,true, 5, "blue");
+
+        fan1.toString();
+        fan2.toString();
+    }
+}
